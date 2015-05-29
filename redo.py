a356acb9009b44308b86a9a5febaea2c4f59cc50
gwhittey23 <gerard.whittey@gmail.com>
2015-05-29 18:48:51 -0400

    Comicscreen redone to use Async transfers.

--- redo.py
+++ redo.py
@@ -1,4 +1,4 @@
-__version__ = '1.2'
+__version__ = '1.00167'
 DEBUG = True
 import kivy
 kivy.require('1.8.0')
@@ -15,15 +15,15 @@ from kivy.uix.image import Image,AsyncImage
 from settingsjson import settings_json
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
-from kivy.uix.button import ButtonBehavior
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
-from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty, StringProperty
 from csvdb.csvdroid_db import build_db
 from csconnector import CsComic,ComicStream
 from kivy.logger import Logger
 import os.path
+import requests
+from requests.exceptions import HTTPError
 
 
 
@@ -33,28 +33,35 @@ class ComicScreen(Screen):
 class SecondScreen(Screen):
     pass
 
-class PageBox(BoxLayout):
-    def __init__(self, **kwargs):
-        kwargs['padding'] = 20
-        kwargs['spaceing'] = 10
-        kwargs['orientation'] = 'horizontal'
-
-        super(PageBox, self).__init__(**kwargs)
-
-class PageButton(Button):
+class ComicScreenBntListItem(Button):
+    id = StringProperty('')
+    image = StringProperty('')
+    title = StringProperty('')
+    label = StringProperty('')
     pass
 
-class PageImage(ButtonBehavior,AsyncImage):
-    def __init__(self, **kwargs):
-        super(PageImage, self).__init__(**kwargs)
-        self.bind(on_press=self.click)
-
-    def click(self,instance):
+    def click(button):
         app = App.get_running_app()
         app.root.manager.current = 'comicscreen'
-        app.root.ids['my_carousel'].index = int(instance.id)
+        app.root.ids['my_carousel'].index = int(button.id)
+
+class LibScreenBntListItem(Button):
+    id = StringProperty('')
+    image = ObjectProperty
+    title = StringProperty('')
+    label = StringProperty('')
+    pass
+
+    def click(button):
+        app = App.get_running_app()
+        app.root.manager.current = 'comicscreen'
+        print button.id
+        app.root.load_comic_screen(int(button.id))
 
 
+
+class ButtonList(GridLayout):
+    pass
 
 class RootWidget(FloatLayout):
 
@@ -63,44 +70,42 @@ class RootWidget(FloatLayout):
     def load_lib_screen(self, *args):
         display_mode = 'Series' #default mode
         id = 96
-        # page_count = 22
-        # base_url = App.get_running_app().config.get('Server', 'url')
-        # base_file = App.get_running_app().config.get('Server', 'storagedir')
-        # grid = GridLayout(cols=4, size_hint=(None,None),spacing=(10,50),padding=10, pos_hint = {.5,.2})
-        # grid.bind(minimum_height=grid.setter('height'))
-        #
-        # for i in range(0,page_count):
-        #     fname='%s/%d_P%d.jpg' %(base_file, id, i)
-        #     src = "%s/comic/%d/page/%d" % (base_url, id, i)
-        #     if  os.path.isfile(fname) == False:
-        #
-        #         try:
-        #             r = requests.get(src)
-        #             r.raise_for_status()
-        #         except HTTPError:
-        #             Logger.critical('HTTPerror for %s' % src )
-        #         else:
-        #         #load images asynchronously
-        #            with open(fname,'w') as f:
-        #                  f.write(r.content)
-        #
-        #     inner_grid = GridLayout(cols=1, rows =2,id='inner_grid'+str(i),size_hint=(None,None),size=(130,200),spacing=5)
-        #     # page_button = LibScreenBntListItem(id=str(96), text='#Page' + str(i), size=(129, 200), size_hint=(None, None),
-        #     #                              image=base_file + '/' + str(id) + '_P' + str(i) + '.jpg',
-        #     #
-        #     #                              )
-        #     page_button = ComicScreenBntListItem(source=src, size=(129, 200), size_hint=(None, None),)
-        #
-        #     inner_grid.add_widget(page_button)
-        #     smbutton = Button(size_hint=(None,None),size=(10,10),text='P%s'%str(i+1),background_color=(0,0,0,0))
-        #     inner_grid.add_widget(smbutton)
-        #     grid.add_widget(inner_grid)
-        #
-        #
-        # scroll = ScrollView( size_hint=(.95,.85), do_scroll_y=True, do_scroll_x=False,
-        #                      pos_hint={'center_x': .5, 'center_y': .5} )
-        # scroll.add_widget(grid)
-        # self.ids['fl1'].add_widget(scroll)
+        page_count = 22
+        base_url = App.get_running_app().config.get('Server', 'url')
+        base_file = App.get_running_app().config.get('Server', 'storagedir')
+        grid = GridLayout(cols=4, size_hint=(None,None),spacing=(10,50),padding=10, pos_hint = {.5,.2})
+        grid.bind(minimum_height=grid.setter('height'))
+
+        for i in range(0,page_count):
+            fname='%s/%d_P%d.jpg' %(base_file, id, i)
+            src = "%s/comic/%d/page/%d" % (base_url, id, i)
+            if  os.path.isfile(fname) == False:
+
+                try:
+                    r = requests.get(src)
+                    r.raise_for_status()
+                except HTTPError:
+                    Logger.critical('HTTPerror for %s' % src )
+                else:
+                #load images asynchronously
+                   with open(fname,'w') as f:
+                         f.write(r.content)
+
+            inner_grid = GridLayout(cols=1, rows =2,id='inner_grid'+str(i),size_hint=(None,None),size=(130,200),spacing=5)
+            page_button = LibScreenBntListItem(id=str(96), text='#Page' + str(i), size=(129, 200), size_hint=(None, None),
+                                         image=base_file + '/' + str(id) + '_P' + str(i) + '.jpg',
+
+                                         )
+            inner_grid.add_widget(page_button)
+            smbutton = Button(size_hint=(None,None),size=(10,10),text='P%s'%str(i+1),background_color=(0,0,0,0))
+            inner_grid.add_widget(smbutton)
+            grid.add_widget(inner_grid)
+
+
+        scroll = ScrollView( size_hint=(.95,.85), do_scroll_y=True, do_scroll_x=False,
+                             pos_hint={'center_x': .5, 'center_y': .5} )
+        scroll.add_widget(grid)
+        self.ids['fl1'].add_widget(scroll)
 
 
     def load_comic_screen(self, comicstream_number):
@@ -122,23 +127,34 @@ class RootWidget(FloatLayout):
             if not os.path.exists(comic_dir):
                 os.makedirs(comic_dir)
             fname='%s/%d/%d_P%d.jpg' %(base_dir, comicstream_number, comicstream_number, i)
-            src_full = "%s/comic/%d/page/%d?max_height=1200#.jpg" % (base_url, comicstream_number, i)
-            src_thumb = "%s/comic/%d/page/%d?max_height=200#.jpg" % (base_url, comicstream_number, i)
-
-
-            image = AsyncImage(source=src_full, allow_stretch=True)
+            src = "%s/comic/%d/page/%d?max_height=1200#.jpg" % (base_url, comicstream_number, i)
+            if  os.path.isfile(fname) == False:
+                print src
+                Logger.info('Getting Server info for %s' % comicstream_number)
+                try:
+                    r = requests.get(src)
+                    r.raise_for_status()
+                except HTTPError:
+                    Logger.critical('HTTPerror for %s' % src )
+                else:
+                #load images asynchronously
+                    with open(fname,'w') as f:
+                        f.write(r.content)
+            image = Image(source=fname, allow_stretch=True)
             carousel.add_widget(image)
+
+            fname2='%s/%d/%d_P%d.png' %(base_dir, comicstream_number, comicstream_number, i)
+            image.export_to_png(fname2)
 
             inner_grid = GridLayout(cols=1, rows =2,id='inner_grid'+str(i),size_hint=(None,None),size=(130,200),
                                     spacing=5)
-
-
-            page_image = PageImage(source=src_thumb,allow_stretch=True,
-            size=(130,200),size_hint=(None, None),id=str(i))
-
-            inner_grid.add_widget(page_image)
-            page_image.parent.bind(pos=self.setter('pos'))
-            page_image.parent.bind(pos=self.setter('pos'))
+            page_button = ComicScreenBntListItem(
+                            id=str(i), text='#Page' + str(i),
+                            size=(130, 200), size_hint=(None, None),
+                            image=base_dir + '/' + str(comicstream_number) + '/' + str(comicstream_number) +
+                            '_P' + str(i) + '.jpg',)
+            
+            inner_grid.add_widget(page_button)
             smbutton = Button(size_hint=(None,None),size=(10,10),text='P%s'%str(i+1),text_color=(0,0,0),
             background_color=(1,1,1,.5))
 
@@ -157,7 +173,7 @@ class RootWidget(FloatLayout):
 
 
 
-class CRDroidApp(App):
+class RedoApp(App):
 
     def build(self):
         self.settings_cls = SettingsWithSidebar
@@ -194,4 +210,4 @@ class CRDroidApp(App):
         comic_list = comicstrem.get_fulldata()
 
 if __name__ == '__main__':
-    CRDroidApp().run()
+    RedoApp().run()
