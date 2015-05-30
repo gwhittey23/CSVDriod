fe7940bc8b257e15d3c1d24a82d513f6a514dbdb
gwhittey23 <gerard.whittey@gmail.com>
2015-05-30 07:58:52 -0400

    clean up

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-__version__ = '1.2'
+__version__ = '1.220003'
 DEBUG = True
 import kivy
 kivy.require('1.8.0')
@@ -19,10 +19,14 @@ from kivy.uix.button import ButtonBehavior
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.carousel import Carousel
+from kivy.uix.scatter import Scatter
 from kivy.properties import ObjectProperty, StringProperty
-from csvdb.csvdroid_db import build_db
-from csconnector import CsComic,ComicStream
+#from csvdb.csvdroid_db import build_db
+#from csconnector import CsComic,ComicStream
 from kivy.logger import Logger
+from kivy.graphics.transformation import Matrix
 import os.path
 
 
@@ -47,14 +51,28 @@ class PageButton(Button):
 class PageImage(ButtonBehavior,AsyncImage):
     def __init__(self, **kwargs):
         super(PageImage, self).__init__(**kwargs)
-        self.bind(on_press=self.click)
+        app = App.get_running_app()
 
     def click(self,instance):
         app = App.get_running_app()
         app.root.manager.current = 'comicscreen'
         app.root.ids['my_carousel'].index = int(instance.id)
 
-
+class ComicScatter(ScatterLayout):
+    def __init__(self, **kwargs):
+        self.zoom_state = 'normal'
+        super(ComicScatter, self).__init__(**kwargs)
+    def on_touch_down(self, touch):
+        if touch.is_double_tap:
+            print self.zoom_state
+            if self.zoom_state == 'zoomed':
+                self.zoom_state = 'normal'
+                mat = self.transform_inv
+                self.apply_transform(mat,anchor=(0,0))
+            elif self.zoom_state == 'normal':
+                self.zoom_state = 'zoomed'
+                mat = Matrix().scale(1.5, 1.5, 1.5)
+                self.apply_transform(mat,anchor=touch.pos)
 
 class RootWidget(FloatLayout):
 
@@ -103,18 +121,21 @@ class RootWidget(FloatLayout):
         # self.ids['fl1'].add_widget(scroll)
 
 
+
     def load_comic_screen(self, comicstream_number):
         carousel = self.ids['my_carousel']
         carousel.clear_widgets()
+
         Logger.debug(str(comicstream_number))
         page_count = 22
         comicstream_number = int(self.ids['txt1'].text)
         id = '96'
-        cscomic = CsComic(comicstream_number)
+       # cscomic = CsComic(comicstream_number)
         base_url = App.get_running_app().config.get('Server', 'url')
         base_dir = 'images'
         #base_file = App.get_running_app().config.get('Server', 'storagedir')
-
+        scroll = ScrollView( size_hint=(1,1), do_scroll_x=True, do_scroll_y=False )
+        self.pop = Popup(id='page_pop',title='Pages', content=scroll, pos_hint ={'y': .0001},size_hint = (1,.33))
         grid = GridLayout(rows=1, size_hint=(None,None),spacing=5,padding_horizontal=5,id='outtergrd')
         grid.bind(minimum_width=grid.setter('width'))
         for i in range(0, page_count):
@@ -124,10 +145,13 @@ class RootWidget(FloatLayout):
             fname='%s/%d/%d_P%d.jpg' %(base_dir, comicstream_number, comicstream_number, i)
             src_full = "%s/comic/%d/page/%d?max_height=1200#.jpg" % (base_url, comicstream_number, i)
             src_thumb = "%s/comic/%d/page/%d?max_height=200#.jpg" % (base_url, comicstream_number, i)
-
-
             image = AsyncImage(source=src_full, allow_stretch=True)
-            carousel.add_widget(image)
+            print image._coreimage.size
+            #expermenatal adding in saving to cache need to implement option turn saving pages on/off
+            #image._coreimage.bind(on_load=self.on_image_loaded)
+            scatter = ComicScatter(do_rotation=False,id='sclay'+str(i),scale_min=1, scale_max=2.)
+            scatter.add_widget(image)
+            carousel.add_widget(scatter)
 
             inner_grid = GridLayout(cols=1, rows =2,id='inner_grid'+str(i),size_hint=(None,None),size=(130,200),
                                     spacing=5)
@@ -139,6 +163,8 @@ class RootWidget(FloatLayout):
             inner_grid.add_widget(page_image)
             page_image.parent.bind(pos=self.setter('pos'))
             page_image.parent.bind(pos=self.setter('pos'))
+            page_image.bind(on_press=self.pop.dismiss)
+            page_image.bind(on_press=page_image.click)
             smbutton = Button(size_hint=(None,None),size=(10,10),text='P%s'%str(i+1),text_color=(0,0,0),
             background_color=(1,1,1,.5))
 
@@ -147,12 +173,23 @@ class RootWidget(FloatLayout):
         carousel.pos_hit = {'top':1}
 
         #Build the popup scroll of page buttons
-        scroll = ScrollView( size_hint=(1,1), do_scroll_x=True, do_scroll_y=False )
-        scroll.add_widget(grid)
-        self.pop = Popup(title='Pages', content=scroll, pos_hint ={'y': .0001},size_hint = (1,.33))
 
+        scroll.add_widget(grid)
+
+        #content.bind(on_press=popup.dismiss)
     def open_pagescroll_popup(self):
         self.pop.open()
+    #going to add in save to disc after asyncimage if done downloading.
+    # def on_image_loaded(self, *args):
+    #     pass
+    def zoom_me(self):
+        carousel = self.ids['my_carousel']
+        print carousel.current_slide.zoom_state
+        # carousel.current_slide.scale = 1.5
+        mat = Matrix().scale(1.5, 1.5, 1.5)
+        carousel.current_slide.apply_transform(mat,anchor=(50,50))
+
+
 
 
 
@@ -165,8 +202,8 @@ class CRDroidApp(App):
         print 'user_data_dir = %s' %self.user_data_dir
         # setting = self.config.get('example', 'url')
         # print setting
-        if  os.path.isfile('cachedb.sqlite') == False:
-            build_db()
+        # if  os.path.isfile('cachedb.sqlite') == False:
+        #    # build_db()
         return RootWidget()
     def build_config(self, config):
         config.setdefaults('Server',
@@ -189,9 +226,9 @@ class CRDroidApp(App):
         print config, section, key, value
 
     def get_full_data(self):
-        comicstrem =ComicStream()
+        # comicstrem =ComicStream()
         Logger.debug('getting full data')
-        comic_list = comicstrem.get_fulldata()
+        # comic_list = comicstrem.get_fulldata()
 
 if __name__ == '__main__':
     CRDroidApp().run()
